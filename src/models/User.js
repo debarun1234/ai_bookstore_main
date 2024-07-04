bd03baff99aeb01e49aa189deb052c2fa3b702c6
Debarun Ghosh <debarun.ghosh.2024@gmail.com>
2024-07-04 20:04:12 +0530

    models update

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 // src/models/User.js
 
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
+const { sequelize } = require('../config/db').default;
 const bcrypt = require('bcryptjs');
 
 const User = sequelize.define('User', {
